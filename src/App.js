39500f4411e0310b71a58aebf44913002ddba61d
parang <appdler96@kaist.ac.kr>
2019-05-13 03:29:55 +0900

    fix bugs

--- src/App.js
+++ src/App.js
@@ -9,11 +9,8 @@ import AppWrapper from "./App.styled"
 function App() {
 	return (
 		<AppWrapper>
-			<Route path="/:route?" component={Header} />
 			<Switch>
-				<Route path="/home" component={HomePage} />
-				<Route path="/main" component={MainPage} />
-				<Route path="/todo" component={TodoList}/>
+				<Route path="/" component={HomePage} />
 			</Switch>
 		</AppWrapper>
 	)
